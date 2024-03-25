17f1a13e09f7133124dab7d9383bee6bd3493603
Lenovo <Lenovo@Julian>
2024-03-25 22:07:53 +0800

    added subpackages for exceptions and classes

--- src/pvz/classes/Bomb.java
+++ src/pvz/classes/Bomb.java
@@ -2,7 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pvz;
+package pvz.classes;
+
+import pvz.classes.Plant;
+import pvz.classes.Tile;
+import pvz.classes.Zombie;
 
 /**
  *
