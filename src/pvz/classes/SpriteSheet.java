49e5117db20d98fdf131ede1999c48799d451d84
Lenovo <Lenovo@Julian>
2024-06-05 02:01:18 +0800

    javadocs operational in dist folder

--- src/pvz/classes/SpriteSheet.java
+++ src/pvz/classes/SpriteSheet.java
@@ -12,20 +12,41 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /**
- *
+ *Contains SpriteSheet image for classes under Entity
  * @author Lenovo
  */
 public class SpriteSheet {
+    /**
+     * image that contains sprites of classes under Entity
+     */
     private BufferedImage image;
     
+    /**
+     * constructor for SpriteSheet
+     * @param i image contained by SpriteSheet
+     */
     public SpriteSheet(BufferedImage i){
         this.image = i;
     }
     
+    /**
+     * grabs a sprite image from a sprite sheet
+     * @param col column of sprite in sprite sheet
+     * @param row row of sprite in sprite sheet
+     * @param width pixel-width of sprite
+     * @param height pixel-height of sprite
+     * @return cut out sprite image
+     */
     public BufferedImage grabImage(int col, int row, int width, int height){
         BufferedImage subImg = image.getSubimage(width*(col - 1), height*(row - 1), width, height);
         return subImg;
     }
+    /**
+     * resize an image to given width and height
+     * @param width target width of image
+     * @param height target height of image
+     * @return resized image with specified width and height
+     */
     public BufferedImage resizeImage(int width, int height){
         BufferedImage scaledImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = scaledImg.createGraphics();
@@ -34,6 +55,13 @@ public class SpriteSheet {
         g.dispose();
         return scaledImg;
     }
+    /**
+     * resize an image to given width and height
+     * @param width target width of image
+     * @param height target height of image
+     * @param buffImg image to be resized
+     * @return resized image with specified width and height
+     */
     public BufferedImage resizeImage(int width, int height, BufferedImage buffImg){
 //        Image img = buffImg.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         BufferedImage scaledImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
--- src/pvz/classes/Zombie.java
+++ src/pvz/classes/Zombie.java
@@ -8,72 +8,118 @@ import java.util.ArrayList;
 import javax.swing.JLabel;
 
 /**
- *
+ * Subclass of Entity that attacks Plants and is attacked by Projectile
  * @author Lenovo
  */
 public class Zombie extends Entity {
+    /**
+     * type of Zombie dictated by a string
+     */
     private String type;
+    /**
+     * hit points of Zombie
+     */
     private int hp;
-    private int atk;
+    /**
+     * attack damage of Zombie
+     */
+    private int attack;
+    /**
+     * current target plant of Zombie
+     */
     private Plant target;
+    /**
+     * amount of updates before an object is moved
+     */
     private int moveInterval;
+    /**
+     * label containing the zombie's ImageIcon
+     */
     private JLabel zombieLabel;
+    /**
+     * ArrayList containing zombies that will be used for a certain game
+     * new instances will have parameters copied from these zombies
+     */
     protected static ArrayList<Zombie> usedZombies = new ArrayList<>();
     
+    /**
+     * constructor for Zombie
+     * @param t type of Zombie
+     * @param h hit points of Zombie
+     * @param a attack damage of Zombie
+     * @param s speed of Zombies measured in seconds per Tile (should be negative)
+     * @param f image filename of Zombie containing spritesheet
+     * @param sw width of sprite
+     * @param sh height of sprite
+     * @param maxF max frames in sprite
+     */
     public Zombie(String t, int h, int a, double s, String f, int sw, int sh, int maxF){
         super(s, f, sw, sh, maxF);
         type = t;
         hp = h;
-        atk = a;
+        attack = a;
     }
     
+    /**
+     * returns JLabel of Zombie sprite
+     * @return JLabel containing Zombie sprite
+     */
     public JLabel getZombieLabel(){
         return zombieLabel;
     }
+    /**
+     * sets Zombie's JLabel
+     * @param l new JLabel
+     */
     public void setZombieLabel(JLabel l){
         zombieLabel = l;
     }
+    /**
+     * sets Zombie's move interval
+     * @param i new update count interval
+     */
     public void setMoveInterval(int i){
         moveInterval = i;
     }
+    /**
+     * increases move interval by 1
+     */
     public void incMoveInterval(){
         moveInterval++;
     }
+    /**
+     * returns hit points of Zombie
+     * @return hit points of Zombie
+     */
     public int getHP(){
         return hp;
     }
+    /**
+     * sets hit points of Zombie
+     * @param h new hit points of Zombie
+     */
     public void setHP(int h){
         hp = h;
     }
+    /**
+     * returns Zombie's move interval
+     * @return Zombie's current move interval
+     */
     public int getMoveInterval(){
         return moveInterval;
     }
     
-    public void targetPlant(){
-        Plant p = Tile.getPlant(xCoord, yCoord);
-        if(p != null){
-            target = p;
-        }
-    }
-    
-    public void eatPlant(){
-//        if(target != null && target.getHP() > 0){
-//            if(target.getHP() > atk){
-//                target.setHP(target.getHP() - atk);
-//            }
-//            else{
-//                //target Plant dies
-//                target.setHP(0);
-//                Tile.removeEntity(target, xCoord, yCoord);
-//                System.out.println("Zombie has eaten plant."); //removable
-//            }
-//        }
-    }
-    
+    /**
+     * add zombies to ArrayList usedZombies
+     * @param z To be added and used as Zombie Instances
+     */
     public static void addUsedZombies(Zombie z){
         usedZombies.add(z);
     }
-    
+    /**
+     * return ArrayList of usedZombies
+     * @return ArrayList of usedZombies
+     */
     public static ArrayList getUsedZombies(){
         return usedZombies;
     }
